8c7f0a3d7267265d8b63fbf76a3cb638eb2e2d3b
Concept <conceptcodes@gmail.com>
2021-07-09 14:48:13 -0400

    first commit

---
+++ videt.py
@@ -0,0 +1,26 @@
+import cv2
+import argparse
+
+class Videt():
+    def __init__(self, input_file, output_file):
+        self.input_file = input_file
+        self.output_file = output_file
+
+    def _load_frames(self):
+        pass
+
+    def _load_audio(self):
+        pass
+
+    def censor_video(obj_list=[]):
+        pass
+
+    def censor_audio(word_list=[]):
+        pass
+
+
+if __name__ == '__main__':
+    print('Wagwan Universe')
+
+
+    
