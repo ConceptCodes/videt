9cb1c7334bddc7c39c3beba1cbf0b742841cdbba
Concept <conceptcodes@gmail.com>
2021-07-09 20:42:49 -0400

    added censor video, havent tested but looks sweet

--- videt.py
+++ videt.py
@@ -2,60 +2,36 @@ import os
 import cv2
 import argparse
 from moviepy.editor import *
+from cvlib.object_detection import YOLO
 
 class Videt():
     def __init__(self, input_file, output_file):
         self.input_file = input_file
         self.output_file = output_file
-        self.video_frames = self._load_frames()
-
-    # ------------ Helper Functions ---------------------
-    def _load_frames(self):
-        tmp =[]
-        cap = cv.VideoCapture(self.input_file)
-        while cap.isOpened():
-            ret, frame = cap.read()
-            if not ret:
-                print("Error with loading video")
-                break
-            tmp.append(frame)
-            if cv.waitKey(1) == ord('q'):
-                break
-        cap.release()
-        return tmp
-
-    def _load_audio(self):
-        pass
-
-    def _detect_objects(self):
-        pass
+        self.audio = self.video.audio
+        self.video = VideoFileClip(self.input_file)
+            
+    def _detect_objects(self, frame):
+        yolo = YOLO('./ml/yolo/yolov3.weights', './ml/yolo/yolov3.cfg', './ml/yolo/coco.names')
+        bbox, label, conf = yolo.detect_objects(frame)
+        return (label, bbox)
     
     def _save_content(self):
-        cap = cv.VideoCapture(0)
-        fourcc = cv.VideoWriter_fourcc(*'XVID')
-        out = cv.VideoWriter(self.output_file, fourcc, 20.0, (640,  480)) #need to grab framse size, will throw in init to prevent writing twice
-        while cap.isOpened():
-            ret, frame = cap.read()
-            if not ret:
-                print("Can't receive frame (stream end?). Exiting ...")
-                break
-            frame = cv.flip(frame, 0)
-            # write the flipped frame
-            out.write(frame)
-            cv.imshow('frame', frame)
-            if cv.waitKey(1) == ord('q'):
-                break
-        # Release everything if job is finished
-        cap.release()
-        out.release()
-    # ------------- Core Algorithm ----------------------
-    def censor_video(obj_list=[]):
         pass
+
+    def censor_video(obj_list=[]):
+        tmp = []
+        for frame in self.video.iter_frames():
+            objects = filter(lambda x: x[0] in obj_list, self._detect_objects(frame))
+            for img in objects:
+                blur = cv2.GaussianBlur(img[1], (51,51), 0) # 51 will need to be replaced with bbox w, h
+                frame[y:y+h, x:x+w] = blur
+                tmp.append(frame)
+        return frame
 
     def censor_audio(word_list=[],sound=""):
         pass
 
-# --------------- Main Function ------------------------
 if __name__ == '__main__':
     print('Wagwan Universe')
 
